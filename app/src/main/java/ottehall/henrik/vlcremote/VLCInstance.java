b1ff00e796419fdc1d4802b49815d343d6ad8000
othe1492 <henot294@gmail.com>
2015-11-23 19:39:44 +0100

    Cleaned up HTTPInterface and added VLCInstance

---
+++ app/src/main/java/ottehall/henrik/vlcremote/VLCInstance.java
@@ -0,0 +1,25 @@
+package ottehall.henrik.vlcremote;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Henrik on 2015-11-23.
+ */
+public class VLCInstance
+{
+    private String m_videoName;
+    private int m_currentTime;
+    private int m_length;
+    private Commands m_command;
+
+    private void parseResponse(InputStream response)
+    {
+
+    }
+}
